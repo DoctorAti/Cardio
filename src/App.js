fef97c125867a2c848e352a4845c5dbb61aa90eb
AnantGowadiya <anant.gowadiya@publicissapient.com>
2020-01-29 16:40:27 +0530

    terms policy footer

--- src/App.js
+++ src/App.js
@@ -7,58 +7,78 @@ import { Working } from './components/working.js';
 import { MyCarousel } from './components/testimonial.js';
 import NavigationBar from './components/navigationBar.js';
 import ContactModal from './components/contactModal.js';
-
 import { Jumbotron } from 'reactstrap';
 
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Terms from './components/terms';
+import Services from './components/services';
+import Footer from './components/footer';
+import ScrollToTop from './components/ScrollToTop';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <ContactModal />
-        <NavigationBar />
-        
-        <div id="introduction">
-          <Introduction />
-        </div>
+      <Router>
+        <ScrollToTop>
+          <Switch>
+            <div>
+              <ContactModal />
+              <NavigationBar />
+              <Route exact path="/">
+                <div id="introduction">
+                  <Introduction />
+                </div>
 
-        <div id="working">
-          <Working />
-        </div>
+                <div id="working">
+                  <Working />
+                </div>
 
-        <div id="testimonial">
-          <MyCarousel />
-        </div>
+                <div id="testimonial">
+                  <MyCarousel />
+                </div>
 
-        <div id="component3">
-           <Loginform /> 
-        </div>
+                <div id="component3">
+                  <Loginform />
+                </div>
 
-        <div id="component4">
-          <Jumbotron>
-            <h1 className="display-3">Joke Cards</h1>
-            <p className="lead">
-              Welcome to the next billion dollar app taking the world by storm
-            </p>
-            <hr className="my-2" />
-            <p>Prepare to be amused!</p>
-            <p className="lead"></p>
-          </Jumbotron>
-        </div>
+                <div id="component4">
+                  <Jumbotron>
+                    <h1 className="display-3">Joke Cards</h1>
+                    <p className="lead">
+                      Welcome to the next billion dollar app taking the world by
+                      storm
+                    </p>
+                    <hr className="my-2" />
+                    <p>Prepare to be amused!</p>
+                    <p className="lead"></p>
+                  </Jumbotron>
+                </div>
 
-        <div id="component5">
-          <Jumbotron>
-            <h1 className="display-3">Joke Cards</h1>
-            <p className="lead">
-              Welcome to the next billion dollar app taking the world by storm
-            </p>
-            <hr className="my-2" />
-            <p>Prepare to be amused!</p>
-            <p className="lead"></p>
-          </Jumbotron>
-        </div>
-      </div>
+                <div id="component5">
+                  <Jumbotron>
+                    <h1 className="display-3">Joke Cards</h1>
+                    <p className="lead">
+                      Welcome to the next billion dollar app taking the world by
+                      storm
+                    </p>
+                    <hr className="my-2" />
+                    <p>Prepare to be amused!</p>
+                    <p className="lead"></p>
+                  </Jumbotron>
+                </div>
+              </Route>
+              <Route exact path="/terms-and-conditions">
+                <Terms />
+              </Route>
+              <Route exact path="/privacy-policy">
+                <Services />
+              </Route>
+              <Footer />
+            </div>
+          </Switch>
+        </ScrollToTop>
+      </Router>
     );
   }
 }
