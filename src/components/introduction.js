c69cd223cc7abc0994378ec1ecce34e8a09e0901
AnantGowadiya <anant.gowadiya@publicissapient.com>
2020-02-06 15:57:47 +0530

    imporved introduction

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-    Row, Col
-} from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 
 import { Jumbotron } from 'reactstrap';
 
@@ -10,60 +8,105 @@ import { Image } from 'react-bootstrap';
 import Doctor from '../resources/frontpic.png'
 import MobileDoctor from '../resources/jeet_doctor_mobile.jpg'
 import {
-    BrowserView,
-    MobileView,
-    isBrowser,
-    isMobile
-} from "react-device-detect";
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from 'react-device-detect';
 
 export default class Introduction extends React.Component {
+  render() {
+    return <div>{this.renderIntrouction()}</div>;
+  }
 
-    render() {
-        return (
-            <div>
-                {this.renderIntrouction()}
-            </div>
-        );
-    }
+  renderIntrouction = () => {
+    if (isMobile) {
+      return (
+        <div>
+          <Jumbotron style={{ background: { MobileDoctor } }}>
+            <p style={{ lineHeight: '10px' }}>
+              <h3>Cardio is hard</h3>
+            </p>
+            <p style={{ lineHeight: '10px', color: 'green' }}>
+              We will help you beat it
+            </p>
 
-    renderIntrouction = () => {
-        if (isMobile) {
-            return <div>
-                <Jumbotron style={{ background: { MobileDoctor } }}>
-
-                    <p style={{ lineHeight: '10px' }}><h3>Cardio is hard</h3></p>
-                    <p style={{ lineHeight: '10px', color:'green' }}>We will help you beat it</p>
-
-                    <p style={{fontFamily: 'Justify'}}>Get a second opinion by <br/> world renowned team of <br/> cardiac specialist</p>
-                    <p className="lead">
-                    </p>
-                </Jumbotron>
-            </div>
-
-        }
-        return (
-            <div style={{backgroundColor: '#ededed'}}>
-        <Row style={{ marginLeft: '7.5%', marginRight: '7.5%', }} > 
-            <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-
-
-                <div className="container">
-                    <div  style={{ verticalAlign: 'text-bottom', paddingTop: '15%', paddingLeft: '17.5%' , textAlign:'middle', fontFamily: 'Verdana', fontSize: '180%', color: '#228b22'}} > 
-                          <span style={{color:'#ff8c00', fontWeight: 'bolder'}}>Cardio-health</span> helps you to
-                          <br /> 
-                          Fight all your <span style={{color:'#ff8c00', fontWeight: 'bolder'}}>cardio</span> problems
-                    </div>
-                    <div  style={{ verticalAlign: 'text-bottom', paddingTop: '5%', paddingLeft: '17.5%' , textAlign:'middle', fontFamily: 'Verdana', fontSize: '130%'}} > 
-                          Get a second opinion by a world renowned team of cardiac specialist
-                    </div>
-                </div>
-            </Col>
-            <Col xs={6} sm={6} md={6} lg={6} xl={6} style={{marginLeft: '-15%'}}>
-                <Image src={Doctor} rounded alt="maja aa gaya" height="640" textAlign='left'/>
-            </Col>
-        </Row>
+            <hr className="my-2" />
+            <p>
+              It uses utility classes for <br />
+              typography and spacing to <br />
+              space content out within the
+              <br /> larger container.
+            </p>
+            <p className="lead"></p>
+          </Jumbotron>
         </div>
-        )
+      );
     }
+    return (
+      // <Row style={{ margin: '7.5%' }}>
+      //     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+      //         <div className="container" style={{ textAlign: 'center' }}>
+      //             <h1 style={{ verticalAlign: 'text-bottom', paddingTop: '20%', paddingLeft: '20%', textAlign: 'middle' }} > Your heart is priceless.<br /> We will help you beat it</h1>
+      //             <h4>
+      //                 Cardio Health.com helps you with all of your Cardiac problems. Cardiology diagnosis and treatment options available at your fingertips.
+      //         </h4>
+      //         </div>
+      //     </Col>
+      //     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+      //         <Image src={Doctor} rounded alt="maja aa gaya" height="640" />
+      //     </Col>
 
-};
+      // </Row>
+      <div className="" style={{ backgroundColor: '#E0ECF7' }}>
+        <Row
+          className="container"
+          style={{ textAlign: 'center', alignItems: 'center' }}
+        >
+          <Col
+            xs={8}
+            sm={8}
+            md={8}
+            lg={8}
+            xl={8}
+            style={{
+              textAlign: 'left',
+              alignItems: 'center',
+              paddingLeft: '25%'
+            }}
+          >
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1>
+              Your
+              <span style={{ color: 'red' }}>&nbsp;Heart&nbsp;</span>
+              is priceless
+            </h1>
+            <h2>
+              We will help you <span style={{ color: 'green' }}>beat it</span>
+            </h2>
+            <h5>
+              Cardio Health.com helps you with all of your Cardiac problems.
+              Cardiology diagnosis and treatment options available at your
+              fingertips.
+            </h5>
+            <br></br>
+            <br></br>
+            <br></br>
+          </Col>
+          <Col
+            xs={4}
+            sm={4}
+            md={4}
+            lg={4}
+            xl={4}
+            style={{ backgroundImage: `url()` }}
+          >
+            <img src={Doc}></img>
+          </Col>
+        </Row>
+      </div>
+    );
+  };
+}
--- src/components/working.js
+++ src/components/working.js
@@ -1,104 +1,99 @@
 import React from 'react';
-import doctor from '../resources/doctor.svg'
-import upload from '../resources/upload.svg'
-import support from '../resources/support.svg'
-import monitor from '../resources/monitor.svg'
+import doctor from '../resources/doctor.svg';
+import upload from '../resources/upload.svg';
+import support from '../resources/support.svg';
+import monitor from '../resources/monitor.svg';
 
 import '../App.css';
 //import Work from './work.js'
-import {
-    Container,
-    Row,
-    Col
-  } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
-  import { Image, Button} from 'react-bootstrap';
-
-
+import { Image, Button } from 'react-bootstrap';
 
 export class Working extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
     this.state = {
       jokes: [
         {
           imaginary: monitor,
-          setup: "Get in touch with us and submit your problem", 
-          
+          setup: 'Get in touch with us and submit your problem'
         },
         {
           imaginary: support,
-          setup: "Our Case Manager will call you and explain you how platform works",
-          
+          setup:
+            'Our Case Manager will call you and explain you how platform works'
         },
         {
           imaginary: upload,
-          setup: "Submit your test reports online",
-          
+          setup: 'Submit your test reports online'
         },
         {
           imaginary: doctor,
-          setup: "Get a second opinion from our team of renowned cardiologists",
-          
+          setup: 'Get a second opinion from our team of renowned cardiologists'
         }
       ]
+    };
+  }
+  render() {
+    let jokeCards = this.state.jokes.map(humor => {
+      return (
+        // <div className="">
+        <Col
+          xs={12}
+          sm={12}
+          md={3}
+          lg={3}
+          xl={3}
+          style={{ marginTop: '2.5%', marginBottom: '2.5' }}
+        >
+          <Work humor={humor} />
+        </Col>
+        // </div>
+      );
+    });
 
-      }
-    }
-    render() {
-        
-        let jokeCards = this.state.jokes.map(humor => {
-            return (
-              // <div className="">
-              <Col xs={12} sm={12} md={3} lg={3} xl={3}  style={{marginTop: '2.5%', marginBottom: '2.5'}}>
-                  <Work humor={humor} />
-              </Col>
-              // </div>
-            )
-          });
-
-          return (
-              <div  style={{margin: '7.5%', textAlign: 'center'}}>
-                <div id="HowItWorks" >
-                    <h1>How It Works?</h1>
-                </div >
-                <Container fluid>
-                <Row>
-                    {jokeCards}
-                </Row>
-                </Container>
-              
-              </div>
-          )
-    }
-    
-};
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <div id="HowItWorks">
+          <br></br>
+          <h1>How It Works?</h1>
+        </div>
+        <Container fluid>
+          <Row>{jokeCards}</Row>
+        </Container>
+      </div>
+    );
+  }
+}
 
 class Work extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { fadeIn: false };
-      }
+  constructor(props) {
+    super(props);
+    this.state = { fadeIn: false };
+  }
 
-    render() {
-        let {imaginary, setup } = this.props.humor;
-        return (
-            <div>
-              <Button 
-              type="button"
-              data-toggle="modal"
-              data-target="#myModal"
-              class="btn btn-default" style={{backgroundColor:'#FFFFFF', borderColor:'white',}}>
-              
-              <div class="container text-center" style={{textAlign: 'center'}}>
-                <Image src={imaginary} style={{height:"10vh", width:"10vh", textAlign:"centre" }} rounded/>
-              </div>
-              <div style={{color:'black', textAlign: 'center'}} >
-                {setup}
-              </div>
-                      
-              </Button>
-            </div>
-        );
-    }
-};
+  render() {
+    let { imaginary, setup } = this.props.humor;
+    return (
+      <div>
+        <Button
+          type="button"
+          data-toggle="modal"
+          data-target="#myModal"
+          class="btn btn-default"
+          style={{ backgroundColor: '#FFFFFF', borderColor: 'white' }}
+        >
+          <div class="container text-center" style={{ textAlign: 'center' }}>
+            <Image
+              src={imaginary}
+              style={{ height: '10vh', width: '10vh', textAlign: 'centre' }}
+              rounded
+            />
+          </div>
+          <div style={{ color: 'black', textAlign: 'center' }}>{setup}</div>
+        </Button>
+      </div>
+    );
+  }
+}
